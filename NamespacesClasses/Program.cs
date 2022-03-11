24e75ca77166f1a22255bcfe0372cad7f36841e3
Marija Brkić Bakarić <mbrkic@uniri.hr>
2022-03-11 07:04:47 +0100

    Add project files.

---
+++ NamespacesClasses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NamespacesClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SimpleMath.Add(2, 3);
+        }
+    }
+
+    class SimpleMath
+    {
+        public static void Add(int x, int y)
+        {
+            rez = x + y;
+            Console.WriteLine(rez);
+        }
+
+        public static void Subtract(int x, int y)
+        {
+            rez = x - y;
+            Console.WriteLine(rez);
+        }
+
+        public static void Multiply(int x, int y)
+        {
+            rez = x * y;
+            Console.WriteLine(rez);
+        }
+
+        public static void Divide(float x, float y)
+        {
+            rez = x / y;
+            Console.WriteLine(rez);
+        }
+
+    }
+}
---
+++ Tjedan2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Tjedan2
+{
+    internal class Program
+    {
+        static double rez;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Add(2, 3);
+            Subtract(2, 3);
+            Multiply(2,3);
+            Divide(2,3);    
+            
+        }
+
+        private static void Add(int x, int y)
+        {
+            rez = x + y;
+            Console.WriteLine(rez); }
+
+        private static void Subtract(int x, int y)
+        {
+            rez = x - y;
+            Console.WriteLine(rez);
+        }
+
+        private static void Multiply(int x, int y)
+        {
+            rez = x * y;
+            Console.WriteLine(rez);
+        }
+
+        private static void Divide(float x, float y)
+        {
+            rez = x / y;
+            Console.WriteLine(rez);
+        }
+    }
+}
